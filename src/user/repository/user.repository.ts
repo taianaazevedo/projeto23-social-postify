66514cdcf86704beb0aa9f39d78ded94cb8732a9
taianaazevedo <taianazev@gmail.com>
2023-07-10 10:44:20 -0300

    feat: create repository

---
+++ src/user/repository/user.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from '../dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async addUser(data: CreateUserDTO) {
+    await this.prisma.user.create({ data: data });
+  }
+
+  async findAllUsers() {
+    return await this.prisma.user.findMany({});
+  }
+
+  async findUserByEmail(email: string) {
+    return await this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async findUserById(id: number) {
+    return await this.prisma.user.findFirst({ where: { id } });
+  }
+}
